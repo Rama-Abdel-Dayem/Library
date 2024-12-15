46ec77dd0b6ccbe386daf13c89893279a86060c0
Rama Abdel Dayem <ramaabdeldayem@gmail.com>
2024-12-15 17:40:23 +0300

    Update LibraryUI.py

--- LibraryUI.py
+++ LibraryUI.py
@@ -4,3 +4,14 @@ from Library_Script import *
 
 
 sl.title("Welcome to the Library System")
+
+
+while(True):
+    option= sl.selectbox('What would you like to look at?', ['All Books', 'Available Books', 'Authors',
+                                                            'Members','Staff','Issue History'])
+    if option =='Members':
+        sl.header('Our Valued Members')
+        
+
+
+
